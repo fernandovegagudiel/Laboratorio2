bd1f55f8e8a2bbcf695e82572f00f509572a8950
FERNANDO JOSE VEGA GUDIEL <fvegag1@miumg.edu.gt>
2025-02-14 01:39:08 -0600

    Add files via upload

---
+++ Parte 1 del lab 2/Parte 1 del lab 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    { 
+
+int numeroE;
+double numeroD;
+string tex;
+bool valBool;
+
+Console.WriteLine("ingrese un número entero: ");
+numeroE = int.Parse(Console.ReadLine());
+Console.WriteLine("ingrese un numero decimal");
+numeroD = double.Parse(Console.ReadLine());
+Console.WriteLine("ingrese un texto");
+tex = Console.ReadLine();
+Console.WriteLine("ingresa un  true/false: ");
+valBool = bool.Parse(Console.ReadLine());
+
+        Console.WriteLine("------los datos ingresados son:-------" );
+
+        Console.WriteLine("El numero entero ingresado es:  "  + numeroE);
+        Console.WriteLine("El numero decimal ingresado es:  "  + numeroD);
+        Console.WriteLine("El texto es :            " + tex);
+        Console.WriteLine("El valor boleano :     " + valBool);
+
+        
+    }
+}
